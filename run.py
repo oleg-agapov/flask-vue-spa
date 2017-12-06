75d57c1fb7502fde8ca5336fe161ca4099923701
Oleg Agapov <oleg.agapov.ua@gmail.com>
2017-12-06 15:25:07 +0200

    Proxying Front-end

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify
 from random import *
 from flask_cors import CORS
+import requests
 
 app = Flask(__name__,
             static_folder = "./dist/static",
@@ -17,4 +18,6 @@ def random_number():
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
+    if app.debug:
+        return requests.get('http://localhost:8080/{}'.format(path)).text
     return render_template("index.html")
