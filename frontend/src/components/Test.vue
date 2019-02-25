0fe28b5a8a4d7a750ecc1269a0338aa2a84d4e8a
Ruben Colomina <ruben.colomina@telespazio.com>
2019-02-25 17:35:26 +0000

    Initial Version

---
+++ frontend/src/components/Test.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <b-table striped hover :items="items" :fields="fields" />
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        // Note 'isActive' is left out and will not appear in the rendered table
+        fields: [
+          {
+            key: 'last_name',
+            sortable: true
+          },
+          {
+            key: 'first_name',
+            sortable: false
+          },
+          {
+            key: 'age',
+            label: 'Person age',
+            sortable: true
+            // Variant applies to the whole column, including the header and footer
+            // variant: 'danger'
+          }
+        ],
+        items: [
+          { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+          { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+          { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+          { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' }
+        ]
+      }
+    }
+  }
+</script>
