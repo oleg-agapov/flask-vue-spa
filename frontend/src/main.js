b0a4d20ac3c30bc1a97c84f161236200ce45bf7f
Ruben Colomina <ruben.colomina@telespazio.com>
2019-02-25 17:34:10 +0000

    Added bootstrapvue and css

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,13 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
